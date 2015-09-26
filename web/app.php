55e47123add7e5d5fd88b8954f6756ed4d768610
Fabian Sabau <fabian.sabau@socialbit.de>
2015-09-26 20:57:20 +0200

    Update front controllers for docker

--- web/app.php
+++ web/app.php
@@ -1,29 +1,27 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+defined('SYMFONY_ENV') || define('SYMFONY_ENV', getenv('SYMFONY_ENV') ?: 'prod');
+defined('SYMFONY_DEBUG') ||
+define('SYMFONY_DEBUG', filter_var(getenv('SYMFONY_DEBUG') ?: SYMFONY_ENV === 'dev', FILTER_VALIDATE_BOOLEAN));
 
-// Enable APC for autoloading to improve performance.
-// You should change the ApcClassLoader first argument to a unique prefix
-// in order to prevent cache key conflicts with other applications
-// also using APC.
-/*
-$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
-$loader->unregister();
-$apcLoader->register(true);
-*/
+if (SYMFONY_DEBUG) {
+    $loader = require_once __DIR__ . '/../app/autoload.php';
+    Debug::enable();
+} else {
+    $loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
+}
 
-require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__ . '/../app/AppKernel.php';
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$kernel = new AppKernel(SYMFONY_ENV, SYMFONY_DEBUG);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
+if (!SYMFONY_DEBUG) {
+    $kernel->loadClassCache();
+}
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
